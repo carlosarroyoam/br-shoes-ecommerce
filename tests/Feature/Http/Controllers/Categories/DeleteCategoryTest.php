75288a4811b1cc905d225568cdafdc70d0ee9389
Carlos Alberto Arroyo Martinez <carlosarroyoam@gmail.com>
2020-09-05 12:00:07 -0500

    Updated test DeleteCategoryTest

--- tests/Feature/Http/Controllers/Categories/DeleteCategoryTest.php
+++ tests/Feature/Http/Controllers/Categories/DeleteCategoryTest.php
@@ -5,61 +5,57 @@ namespace Tests\Feature;
 use App\Category;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use JMac\Testing\Traits\AdditionalAssertions;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class DeleteCategoryTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, AdditionalAssertions;
 
     /**
-     * An admin can delete categories.
+     * Destroy action deletes and redirects to index for an admin user.
      *
      * @return void
      */
-    public function test_an_admin_can_delete_categories()
+    public function test_destroy_deletes_and_redirects_for_admin_users()
     {
-        $user = factory(User::class)->states('is_admin')->make();
+        $user = factory(User::class)->states('is_admin')->create();
         $this->actingAs($user);
         $category = factory(Category::class)->create();
 
-        $response = $this->deleteJson(route('categories.destroy', $category));
+        $response = $this->delete(route('categories.destroy', $category));
 
         $response->assertRedirect(route('categories.index'));
-        $this->assertDeleted('categories', [
-            'id' => $category->id,
-            'name' => $category->name,
-            'slug' => $category->slug,
-        ]);
+        $this->assertDeleted($category);
     }
 
     /**
-     * An authenticated non-admin user cannot delete categories.
+     * Destroy action doesn't delete for an authenticated non-admin user.
      *
      * @return void
      */
-    public function test_a_user_cannot_delete_categories()
+    public function test_destroy_dont_deletes_for_non_admin_users()
     {
-        $user = factory(User::class)->make();
+        $user = factory(User::class)->create();
         $this->actingAs($user);
         $category = factory(Category::class)->create();
 
-        $response = $this->deleteJson(route('categories.destroy', $category));
+        $response = $this->delete(route('categories.destroy', $category));
 
         $response->assertStatus(Response::HTTP_FORBIDDEN);
     }
 
     /**
-     * An unauthenticated user cannot delete categories.
+     * Destroy action doesn't deletes for an unauthenticated user.
      *
      * @return void
      */
-    public function test_an_unauthenticated_user_cannot_delete_categories()
+    public function test_destroy_dont_deletes_for_non_authenticated_users()
     {
         $category = factory(Category::class)->create();
 
-        $response = $this->deleteJson(route('categories.destroy', $category));
+        $response = $this->delete(route('categories.destroy', $category));
 
         $response->assertStatus(Response::HTTP_FORBIDDEN);
     }
