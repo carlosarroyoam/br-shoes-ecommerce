9dbda3914d04ad87d3a59a0076c49cba709826dc
Carlos Alberto Arroyo Martinez <carlosarroyoam@gmail.com>
2020-09-25 21:21:38 -0500

    Added customer dashboard layout

---
+++ modules/customer/Resources/Views/layouts/dashboard.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="robots" content="noindex, nofollow, noarchive">
+
+    <!-- Favicon/App icons -->
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon" />
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+
+    <livewire:styles />
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}" />
+</head>
+
+<body class="font-sans antialiased">
+
+    <!-- Page Content -->
+    <main class="container flex-grow">
+        {{ $slot }}
+    </main>
+
+    <livewire:scripts />
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.0/dist/alpine.min.js" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</body>
+
+</html>
